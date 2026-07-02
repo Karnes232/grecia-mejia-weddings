4265a68688c80814b1bf5c6061568a7eeecaa274
James Karnes <karnes.james@gmail.com>
2026-07-01 20:58:51 -0400

    Remove JournalNewsletter component and update JournalPage exports: Eliminate the newsletter functionality from the JournalPage, streamlining the component structure and enhancing content management.

--- src/components/JournalPage/index.ts
+++ src/components/JournalPage/index.ts
@@ -4,4 +4,4 @@ export { JournalFeatured } from "./JournalFeatured";
 export { JournalCategoryFilter } from "./JournalCategoryFilter";
 export { JournalGrid } from "./JournalGrid";
 export { JournalLoadMore } from "./JournalLoadMore";
-export { JournalNewsletter } from "./JournalNewsletter";
+
